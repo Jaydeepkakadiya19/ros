921490fa63b6aef02d5268d8e4e1d30713a4fbbd
Jaydeep <jaykakadiya19@gmail.com>
2020-08-22 14:41:30 +0530

    data in tex

--- catkin_ws/src/check_odometry/src/check_odom.py
+++ catkin_ws/src/check_odometry/src/check_odom.py
@@ -4,8 +4,23 @@ import rospy
 from tf2_msgs.msg import TFMessage
 
 def callback(msg):
-    print msg
+    file1.write(str(msg))
+    rospy.loginfo( "I heard %s", msg)
+    # print msg
+    
+
+file1=open("data.tex","w")
+st="\n \n \n --------------------------------------------------------- \n \n \n "
 
 rospy.init_node('check_odometry')
 odom_sub = rospy.Subscriber('tf',TFMessage, callback)
-rospy.spin()
+n=1
+while not rospy.core.is_shutdown():
+    rospy.rostime.wallsleep(1)
+    n=n-1
+    if(n==0):
+        break
+
+# print(my_data)
+file1.write(st)
+# file1.close()
